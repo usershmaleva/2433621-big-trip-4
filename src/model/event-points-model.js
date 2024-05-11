85b98b61f99ec581fdcadbe6c94d307377402853
usershmaleva <o.shmaleva@internet.ru>
2024-05-11 15:25:07 +0500

    Большие перемены1

---
+++ src/model/event-points-model.js
@@ -0,0 +1,13 @@
+export default class PointsModel {
+    #service = null;
+    #points = null;
+  
+    constructor(service) {
+      this.#service = service;
+      this.#points = this.#service.points;
+    }
+  
+    get() {
+      return this.#points;
+    }
+  }
