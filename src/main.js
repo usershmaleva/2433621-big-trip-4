40170510d9a8a54fbc59b6abcf929cb2a9a1b7a3
usershmaleva <o.shmaleva@internet.ru>
2024-05-14 21:32:25 +0500

    6.2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import PointsModel from './model/points-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
-import PointsPresenter from './presenter/points-presenter.js';
+import RoutePresenter from './presenter/route-presenter.js';
 import FiltersPresenter from './presenter/filters-presenter.js';
 import TripInfoPresenter from './presenter/trip-info-presenter.js';
 import MockService from './service/mock-service.js';
+import CreatePointPresenter from './presenter/create-point-presenter.js';
 
 const mockService = new MockService();
 const pointsModel = new PointsModel(mockService);
@@ -12,17 +13,29 @@ const offersModel = new OffersModel(mockService);
 const destinationsModel = new DestinationsModel(mockService);
 
 const pointsContainer = document.querySelector('.trip-events');
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const tripMainContainer = document.querySelector('.trip-main');
 
-const pointsPresenter = new PointsPresenter({
-  pointsContainer,
+const createPointPresenter = new CreatePointPresenter({
+  container: tripMainContainer,
+  editorContainer: pointsContainer,
+  pointsModel,
+  offersModel,
+  destinationsModel,
+});
+
+const routePresenter = new RoutePresenter({
+  container: pointsContainer,
+  createPointBtnContainer: tripMainContainer,
   pointsModel,
   offersModel,
   destinationsModel
 });
 
-const filtersPresenter = new FiltersPresenter({ pointsModel });
-const tripInfoPresenter = new TripInfoPresenter();
+const filtersPresenter = new FiltersPresenter({ container: filtersContainer, pointsModel });
+const tripInfoPresenter = new TripInfoPresenter(tripMainContainer);
 
-pointsPresenter.init();
+createPointPresenter.init();
+routePresenter.init();
 filtersPresenter.init();
 tripInfoPresenter.init();
