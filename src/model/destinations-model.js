92efd27279331e5fdcc82258a9087685c00f4c36
usershmaleva <o.shmaleva@internet.ru>
2024-05-14 21:58:00 +0500

    7.10

--- src/model/destinations-model.js
+++ src/model/destinations-model.js
@@ -1,17 +1,36 @@
-export default class DestinationsModel {
-    #service = null;
-    #destinations = null;
-  
-    constructor(service) {
-      this.#service = service;
-      this.#destinations = this.#service.destinations;
-    }
-  
-    get() {
-      return this.#destinations;
-    }
-  
-    getById(id) {
-      return this.#destinations.find((destination) => destination.id === id);
-    }
+import Observable from '../framework/observable.js';
+import { deleteItem, updateItem } from '../utils.js';
+
+export default class DestinationsModel extends Observable {
+  #service = null;
+  #destinations = null;
+
+  constructor(service) {
+    super();
+    this.#service = service;
+    this.#destinations = this.#service.destinations;
   }
+
+  get() {
+    return this.#destinations;
+  }
+
+  getById(id) {
+    return this.#destinations.find((destination) => destination.id === id);
+  }
+
+  add(type, destination) {
+    this.#destinations.push(type, destination);
+    this._notify(destination);
+  }
+
+  update(type, destination) {
+    this.#destinations = updateItem(this.#destinations, destination);
+    this._notify(type, destination);
+  }
+
+  delete(type, destination) {
+    this.#destinations = deleteItem(this.#destinations, destination);
+    this._notify(type, destination);
+  }
+}
--- src/model/offers-model.js
+++ src/model/offers-model.js
@@ -1,17 +1,36 @@
-export default class OffersModel {
-    #service = null;
-    #offers = null;
-  
-    constructor(service) {
-      this.#service = service;
-      this.#offers = this.#service.offers;
-    }
-  
-    get() {
-      return this.#offers;
-    }
-  
-    getByType(type) {
-      return this.#offers.find((offers) => offers.type === type.toLowerCase()).offers;
-    }
+import Observable from '../framework/observable.js';
+import { deleteItem, updateItem } from '../utils.js';
+
+export default class OffersModel extends Observable {
+  #service = null;
+  #offers = null;
+
+  constructor(service) {
+    super();
+    this.#service = service;
+    this.#offers = this.#service.offers;
   }
+
+  get() {
+    return this.#offers;
+  }
+
+  getByType(type) {
+    return this.#offers.find((offers) => offers.type === type.toLowerCase()).offers;
+  }
+
+  add(type, offer) {
+    this.#offers.push(offer);
+    this._notify(type, offer);
+  }
+
+  update(type, offer) {
+    this.#offers = updateItem(this.#offers, offer);
+    this._notify(type, offer);
+  }
+
+  delete(type, offer) {
+    this.#offers = deleteItem(this.#offers, offer);
+    this._notify(type, offer);
+  }
+}
